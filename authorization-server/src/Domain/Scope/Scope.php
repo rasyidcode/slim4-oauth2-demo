98caac0d1272ed3c1437f6c976db1a79af59efaf
rasyidcode <rasyidcode7@gmail.com>
2025-02-09 13:33:18 +0700

    update object domain

--- authorization-server/src/Domain/Scope/Scope.php
+++ authorization-server/src/Domain/Scope/Scope.php
@@ -5,8 +5,8 @@ declare(strict_types=1);
 namespace App\Domain\Scope;
 
 use League\OAuth2\Server\Entities\ScopeEntityInterface;
-use League\OAuth2\Server\Entities\Traits\EntityTrait;
-use League\OAuth2\Server\Entities\Traits\ScopeTrait;
+// use League\OAuth2\Server\Entities\Traits\EntityTrait;
+// use League\OAuth2\Server\Entities\Traits\ScopeTrait;
 
 class Scope implements ScopeEntityInterface
 {
